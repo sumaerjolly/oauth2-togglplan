e8f9fdfb2bb00f940709a7096baea4b966e4cfa1
Sumaer Jolly <sumaerjolly@gmail.com>
2022-03-11 18:05:08 +0530

    add connection logic

---
+++ src/TogglPlanUser.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SumaerJolly\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use League\OAuth2\Client\Tool\ArrayAccessorTrait;
+
+class TogglPlanUser implements ResourceOwnerInterface
+{
+  use ArrayAccessorTrait;
+
+  /**
+   * @var array
+   */
+  protected $response;
+
+  /**
+   * @param array $response
+   */
+  public function __construct(array $response)
+  {
+    $this->response = $response;
+  }
+
+  public function getId()
+  {
+    $this->getValueByKey($this->response, 'id');
+  }
+  public function getEmail()
+  {
+    return $this->getValueByKey($this->response, 'email');
+  }
+
+  public function toArray()
+  {
+    return $this->response;
+  }
+}
